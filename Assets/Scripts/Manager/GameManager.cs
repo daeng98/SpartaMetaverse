c6681c8b475139c9573879b437c227700c0eb3c0
daeng98 <daeng9821@gmail.com>
2025-02-18 16:14:44 +0900

    [Refactor] 미니게임 씬 다시 구성

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static GameManager gm;
+    public static GameManager Instance { get { return gm; } }
+
+    private int currentScore = 0;
+
+    UiManager um;
+    public UiManager UiManager { get { return um; } }
+
+    private void Awake()
     {
-        
+        gm = this;
+        um = FindObjectOfType<UiManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
-        
+        um.UpdateScore(0);
+    }
+
+    public void GameOver()
+    {
+        um.SetRestart();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // ¾À ºÒ·¯¿À±â
+    }
+
+    public void AddScore(int score)
+    {
+        currentScore += score;
+        um.UpdateScore(currentScore);
     }
 }
